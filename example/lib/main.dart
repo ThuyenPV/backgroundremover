e15448e2cb3befceffaff982064e9e3b684e0fb8
ThuyenPV <thuyenpham.dev@gmail.com>
2025-01-24 00:19:22 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterbackgroundremover/backgroundremover.dart';
+import 'package:image_editor/image_editor.dart';
+import 'package:image_picker/image_picker.dart' as ip;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,46 +15,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +34,176 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Uint8List? imageSelected;
+  late ImageEditorController _editorController;
+  late GlobalKey<ExtendedImageEditorState> editorGlobalKey;
+  String? imageFromGallery;
+  Uint8List? afterRemoveBackground;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    _editorController = ImageEditorController();
+    editorGlobalKey = GlobalKey<ExtendedImageEditorState>();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            IconButton(
+              onPressed: () async {
+                final imageFile = await ip.ImagePicker().pickImage(
+                  source: ip.ImageSource.gallery,
+                );
+                imageFromGallery = imageFile?.path;
+                setState(() {});
+              },
+              icon: Icon(Icons.add_a_photo),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            if (imageFromGallery != null)
+              ExtendedImage.file(
+                File(imageFromGallery!),
+                fit: BoxFit.contain,
+                mode: ExtendedImageMode.editor,
+                extendedImageEditorKey: editorGlobalKey,
+                cacheRawData: true,
+                initEditorConfigHandler: (state) {
+                  return EditorConfig(
+                    cropAspectRatio: 1.0,
+                    maxScale: 4.0,
+                    cropRectPadding: const EdgeInsets.all(20.0),
+                    hitTestSize: 20.0,
+                    initCropRectType: InitCropRectType.imageRect,
+                    controller: _editorController,
+                  );
+                },
+              )
+            else
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text('Select an image to begin'),
+              ),
+            Visibility(
+              visible: afterRemoveBackground != null,
+              child: Image.memory(
+                afterRemoveBackground!,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                imageSelected = await onCropImage();
+                if (imageSelected != null) {
+                  final shouldRemoveBackground =
+                      await showBackgroundRemovalDialog(context);
+                  if (shouldRemoveBackground == true) {
+                    final tempDir = await getTemporaryDirectory();
+                    final tempFile =
+                        await File('${tempDir.path}/temp_image.png')
+                            .writeAsBytes(imageSelected!);
+
+                    final res = await FlutterBackgroundRemover.removeBackground(
+                      tempFile,
+                    );
+                    afterRemoveBackground = res;
+                    setState(() {});
+
+                    // if (context.mounted) {
+                    //   GoRouter.of(context).pop(res);
+                    // }
+
+                    await tempFile.delete();
+                  } else {
+                    // if (context.mounted) {
+                    //   GoRouter.of(context).pop(imageSelected);
+                    // }
+                  }
+                }
+              },
+              icon: Icon(Icons.add),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<bool?> showBackgroundRemovalDialog(BuildContext context) {
+    return showModalBottomSheet<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Do you want to remove background?",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context, true);
+                    },
+                    child: const Text("Yes"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context, false);
+                    },
+                    child: const Text("No"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<Uint8List?> onCropImage() async {
+    final ExtendedImageEditorState? state = editorGlobalKey.currentState;
+    if (state == null) return null;
+
+    final imgCropRect = state.getCropRect();
+    final EditActionDetails action = state.editAction!;
+    final double radian = action.rotateRadians;
+
+    final bool flipHorizontal = action.flipY;
+    final bool flipVertical = action.flipY;
+
+    final currentImageSelected = state.rawImageData;
+    final ImageEditorOption option = ImageEditorOption();
+
+    option.addOption(ClipOption.fromRect(imgCropRect ?? Rect.zero));
+    option.addOption(
+        FlipOption(horizontal: flipHorizontal, vertical: flipVertical));
+    if (action.hasRotateDegrees) {
+      option.addOption(RotateOption(radian.toInt()));
+    }
+
+    // option.addOption(ColorOption.saturation(sat));
+    // option.addOption(ColorOption.brightness(bright));
+    // option.addOption(ColorOption.contrast(con));
+
+    option.outputFormat = const OutputFormat.png(88);
+
+    final result = await ImageEditor.editImage(
+      image: currentImageSelected,
+      imageEditorOption: option,
+    );
+    return result;
   }
 }
--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import apple_vision_selfie
+import file_selector_macos
+import image_editor_common
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppleVisionSelfiePlugin.register(with: registry.registrar(forPlugin: "AppleVisionSelfiePlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  ImageEditorPlugin.register(with: registry.registrar(forPlugin: "ImageEditorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
